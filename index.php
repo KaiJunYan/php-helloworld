9f2a1221455ff130549b17069d7c551b5f852537
root <root@master.example.com>
2018-10-17 15:59:51 +0800

    exam

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World, This is Openshift </p>'; ?> 
+ <?php 
+
+ echo '<p>Hello World, This is Openshift </p>'; 
+ echo '<p>Hello World, This is exam  </p>'; 
+ 
+
+?> 
  </body>
 </html>
